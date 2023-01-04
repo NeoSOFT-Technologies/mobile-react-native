1a18092221debb969afab881155de0bd7c225172
faiyyazs <faiyyazshaikh2010@gmail.com>
2023-01-04 20:00:21 +0530

    Multiple entry points for flavors

--- packages/foundation/src/flavors/flavor_config.ts
+++ packages/foundation/src/flavors/flavor_config.ts
@@ -2,41 +2,43 @@ import { Flavor } from './flavor'
 import { FlavorValues } from './flavor_values'
 
 export interface IFlavorConfig {
-  flavor: Flavor
+  flavorObj: Flavor
   values: FlavorValues
 }
 
 export class FlavorConfig {
-  readonly flavor: Flavor
-  readonly values: FlavorValues
+  flavorType: Flavor
+  values: FlavorValues
 
   private static _instance: FlavorConfig
   private static _initialized = false
 
-  private constructor(params: IFlavorConfig) {
-    this.flavor = params.flavor
+  constructor(params: IFlavorConfig) {
+    this.flavorType = params.flavorObj
     this.values = params.values
   }
 
   public static initialize(params: IFlavorConfig): FlavorConfig {
     if (!this._initialized) {
-      FlavorConfig._instance = new FlavorConfig({ flavor: params.flavor, values: params.values })
+      FlavorConfig.instance = new FlavorConfig(params)
       this._initialized = true
+      console.log("Falvor initliazed is ",this._initialized)
+      console.log("Falvor set is ",FlavorConfig.instance.flavorType)
       return FlavorConfig._instance
     }
     return this._instance
   }
 
   static isProd(): boolean {
-    return FlavorConfig._instance.flavor == Flavor.prod
+    return FlavorConfig._instance.flavorType == Flavor.prod
   }
 
   static isQA(): boolean {
-    return FlavorConfig._instance.flavor == Flavor.qa
+    return FlavorConfig._instance.flavorType == Flavor.qa
   }
 
   static isDEV(): boolean {
-    return FlavorConfig._instance.flavor == Flavor.dev
+    return FlavorConfig._instance.flavorType == Flavor.dev
   }
 
   static instance: FlavorConfig = FlavorConfig._instance
--- packages/mobile/src/screen/index.tsx
+++ packages/mobile/src/screen/index.tsx
@@ -1,11 +1,14 @@
 import i18n from 'localisation'
+import { FlavorConfig } from 'foundation'
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
 const Screen = () => {
+
+  
   return (
     <SafeAreaView>
-      <Text>HEllo World</Text>
+      <Text>{`Hello ${""}` }</Text>
       <Text>Hello {i18n.t('appName')}</Text>
     </SafeAreaView>
   )
