e8990927742102fc48ac538bf241359d1f611664
faiyyazs <faiyyazshaikh2010@gmail.com>
2023-01-06 15:28:49 +0530

    App Secrets import name correction

--- packages/foundation/src/foundation.ts
+++ packages/foundation/src/foundation.ts
@@ -2,6 +2,6 @@ import { FlavorConfig } from './flavors/flavor_config'
 import { FlavorValues } from './flavors/flavor_values'
 import { Flavor } from './flavors/flavor'
 
-import { AppSecrets } from './secrets/app_screts'
+import { AppSecrets } from './secrets/app_secrets'
 
 export { FlavorConfig, FlavorValues, Flavor, AppSecrets }
