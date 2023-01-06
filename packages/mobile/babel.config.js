c648fc991215296d36bf594d9f2e7d3505147539
Mohan29997 <kotakmohan33@gmail.com>
2023-01-06 11:54:21 +0530

    removed unused package

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,11 +1,5 @@
 /* eslint-disable no-undef */
 module.exports = {
     presets: ['module:metro-react-native-babel-preset', 
-    ['@babel/preset-typescript', {'onlyRemoveTypeImports': true}]
 ],
-    plugins: [
-            ['@babel/plugin-proposal-decorators', {legacy: true}],
-            ['@babel/plugin-proposal-class-properties', { legacy: true }],
-            'babel-plugin-parameter-decorator'
-          ]
 };
