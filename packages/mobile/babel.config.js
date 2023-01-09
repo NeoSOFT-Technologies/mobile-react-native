42bb4689c6be032dec6ae8403b61caf83b011634
Mohan29997 <kotakmohan33@gmail.com>
2023-01-09 13:53:45 +0530

    add plugin to babel.config file was not allowing to work with decoration

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset']
+  presets: ['module:metro-react-native-babel-preset'],
+  "plugins": [
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+]
 }
