d990c861efe3861a69dfb8ce472ab48b608e98a5
Mohan29997 <kotakmohan33@gmail.com>
2023-01-12 19:56:51 +0530

    network package add and tsretrofit inside it add formatter and error

---
+++ packages/network-retrofit/__tests__/network-retrofit.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+'use strict';
+
+const networkRetrofit = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(networkRetrofit(), 'Hello from networkRetrofit');
+console.info("networkRetrofit tests passed");
---
+++ packages/network-retrofit/src/services/retrofit_service.ts
@@ -0,0 +1,32 @@
+import { BasePath, BaseService, Body, DELETE, GET, Header, PATCH, Path, POST, PUT, Response, ServiceBuilder } from 'ts-retrofit'
+
+interface User {
+    id?: number;
+}
+
+@BasePath("")
+class RetrofitService extends BaseService {
+    @GET("")
+    async get(@Header("Authorization") authorization: string): Promise<Response<Array<User>>> { return <Response<Array<User>>> {} }
+  
+    @POST("")
+    async create(@Header("Authorization") authorization: string, @Body user: User): Promise<Response> { return <Response> {} }
+  
+    @PUT("")
+    async update(@Header("Authorization") authorization: string, @Path("") userId: number, @Body user: User): Promise<Response> { return <Response> {} }
+  
+    @PATCH("")
+    async patch(@Header("Authorization") authorization: string, @Path("") userId: number, @Body user: Partial<User>): Promise<Response> { return <Response> {} }
+  
+    @DELETE("")
+    async delete(@Header("Authorization") authorization: string, @Path("") userId: number): Promise<Response> { return <Response> {} }
+}
+(async () => {
+    const authorization = "";
+    const userService = new ServiceBuilder()
+      .setEndpoint("")
+      .build(RetrofitService);
+    const response = await userService.get(authorization);
+    return response
+  })()
+export default RetrofitService
