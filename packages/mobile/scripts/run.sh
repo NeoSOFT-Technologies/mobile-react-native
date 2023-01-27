4f241fb6dfe579ebbaa11b6f67f828a49149c757
faiyyazs <faiyyazshaikh2010@gmail.com>
2023-01-27 14:13:50 +0530

    correction run script for ios

--- packages/mobile/scripts/run.sh
+++ packages/mobile/scripts/run.sh
@@ -2,28 +2,40 @@
 ENTRYPOINT=""
 APPSUFFIX=""
 OS=""
-
-case $1 in
-android)
-OS="android";;
-ios)
-OS="ios";;
-esac
+COMMAND="yarn run:"
 
 case $2 in
 dev) 
 ENTRYPOINT="entrypoints/main_dev.ts"
 APPSUFFIX="dev"
+COMMAND="$COMMAND$1:$2"
 ;;
 qa) 
 ENTRYPOINT="entrypoints/main_qa.ts"
 APPSUFFIX="qa"
+COMMAND="$COMMAND$1:$2"
 ;;
-prod) ENTRYPOINT="entrypoints/main_prod.ts";;
+prod) 
+ENTRYPOINT="entrypoints/main_prod.ts"
+APPSUFFIX=""
+COMMAND="$COMMAND$1:$2"
+;;
+esac
+
+case $1 in
+android)
+OS="android"
+COMMAND="$COMMAND --appIdSuffix=$APPSUFFIX"
+;;
+
+ios)
+OS="ios"
+COMMAND="$COMMAND"
+;;
 esac
 
 echo "************************************************************************************************************"
-echo "Running yarn run:$1:$2 --appIdSuffix=$APPSUFFIX"
+echo "Running $COMMAND"
 echo "************************************************************************************************************"
 
-yarn run:$1:$2 --appIdSuffix=$APPSUFFIX
+eval $COMMAND
