908607d368475060b65c261e9afd6e36c3f4ff45
Mohan29997 <kotakmohan33@gmail.com>
2023-02-01 18:23:54 +0530

    trying for transformation

--- packages/database-watermelon/src/database_adapter.ts
+++ packages/database-watermelon/src/database_adapter.ts
@@ -1,14 +1,17 @@
 import { MyDatabaseModel } from './../../shared/src/model/mydatabasemodel'
 import { Database } from '@nozbe/watermelondb'
 import { DatabasePort } from 'data'
+
 class DatabaseAdapter implements DatabasePort {
-  readonly database: Database
+  readonly databases: Database
+  data
   constructor(params: { databases: Database }) {
-    this.database = params.databases
+    this.databases = params.databases
   }
 
   async yourFirstDatabaseCall(): Promise<MyDatabaseModel> {
-    return
+     this.data = (this.databases.get('post').query().fetch())
+    return this.data.transform()
   }
 }
 export default DatabaseAdapter
---
+++ packages/database-watermelon/src/model/my_database_response.ts
@@ -0,0 +1,14 @@
+import { BaseLayerDataTransformer, MyDatabaseModel } from 'packages/shared/src/shared';
+
+export abstract class MyDatabaseResponse implements BaseLayerDataTransformer<MyDatabaseResponse, MyDatabaseModel> {
+ post: unknown;
+ getPost:boolean
+
+  restore(data: MyDatabaseModel): MyDatabaseResponse {
+    throw new Error('Database not implemented.')
+  }
+  
+  transform(): MyDatabaseModel {
+    return new MyDatabaseModel({ databaseStatus: this.getPost })
+  }
+}
