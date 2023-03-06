e714864003a93b6f9a49834263995c09cfcd3238
Mohan29997 <kotakmohan33@gmail.com>
2023-03-06 12:32:45 +0530

    remove file which was causing error in main branch

--- packages/mobile/src/screen/index.tsx
+++ packages/mobile/src/screen/index.tsx
@@ -3,7 +3,6 @@ import { FlavorConfig } from 'foundation'
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import ApplicationGraph from '../di/applicationgraph'
-import { SplashPage } from '../feature/splash/splash_page'
 
 const Screen = () => {
   const a = new ApplicationGraph()
@@ -11,7 +10,6 @@ const Screen = () => {
     <SafeAreaView>
       <Text>{`Hello ${FlavorConfig.instance.flavorType}`}</Text>
       <Text>Hello {i18n.t('appName')}</Text>
-      <SplashPage></SplashPage>
     </SafeAreaView>
   )
 }
